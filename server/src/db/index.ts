f13860b5a05edf3885830ae2f15bfe2084c7264f
Alex Yang <guang.alex.yang@gmail.com>
2026-05-24 23:36:32 -0700

    fix: correct EXPOSE port in Dockerfile, add pg pool error handler

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -4,4 +4,8 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+pool.on('error', (err) => {
+  console.error('Unexpected pg pool error', err);
+});
+
 export default pool;
