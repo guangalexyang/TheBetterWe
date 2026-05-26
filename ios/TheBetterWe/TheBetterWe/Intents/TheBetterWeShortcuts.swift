0d94785a9808b68b19fc9a1a412980f29e53e038
Alex Yang <guang.alex.yang@gmail.com>
2026-05-25 23:09:15 -0700

    feat: add RecordPointsIntent shortcut phrases

--- ios/TheBetterWe/TheBetterWe/Intents/TheBetterWeShortcuts.swift
+++ ios/TheBetterWe/TheBetterWe/Intents/TheBetterWeShortcuts.swift
@@ -6,7 +6,10 @@ struct TheBetterWeShortcuts: AppShortcutsProvider {
             intent: AddPointsIntent(),
             phrases: [
                 "Add points in \(.applicationName)",
-                "Award points in \(.applicationName)"
+                "Award points in \(.applicationName)",
+                "在\(.applicationName)加分",
+                "用\(.applicationName)加分",
+                "给孩子加分在\(.applicationName)"
             ],
             shortTitle: "Add Points",
             systemImageName: "plus.circle"
@@ -15,10 +18,23 @@ struct TheBetterWeShortcuts: AppShortcutsProvider {
             intent: DeductPointsIntent(),
             phrases: [
                 "Deduct points in \(.applicationName)",
-                "Remove points in \(.applicationName)"
+                "Remove points in \(.applicationName)",
+                "在\(.applicationName)扣分",
+                "用\(.applicationName)扣分",
+                "给孩子扣分在\(.applicationName)"
             ],
             shortTitle: "Deduct Points",
             systemImageName: "minus.circle"
+        )
+        AppShortcut(
+            intent: RecordPointsIntent(),
+            phrases: [
+                "用\(.applicationName)记分",
+                "Record points in \(.applicationName)",
+                "Log points in \(.applicationName)"
+            ],
+            shortTitle: "Record Points",
+            systemImageName: "mic.circle"
         )
     }
 }
